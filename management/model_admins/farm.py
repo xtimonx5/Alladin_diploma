2ca262528b5c0b33e77eacfbf6cd50f1724c24e5
Dmitry Gavrilenko <d.gavrilenko@invento.by>
2017-05-22 09:51:02 +0300

    foo

--- management/model_admins/farm.py
+++ management/model_admins/farm.py
@@ -19,16 +19,16 @@ class AnimalInline(TabularInline):
             sum += item.weight
         return sum if yield_objs.exists() else '-'
 
-    def yields_percent(self,obj):
+    def yields_percent(self, obj):
         yield_objs = MilkYield.objects.filter(animal=obj)
         sum = 0.0
         for item in yield_objs:
             sum += item.weight
-        return sum/yield_objs.count() if yield_objs.exists() else '-'
+        return sum / yield_objs.count() if yield_objs.exists() else '-'
 
     readonly_fields = (
         'yields_sum',
-        'yields_percent'
+        'yields_percent',
     )
 
     fields = (
@@ -63,13 +63,205 @@ class FarmAdmin(ModelAdmin):
             company = Company.objects.get(owner=request.user)
             return Farm.objects.filter(company=company)
 
+    def krahmal_needs(self, obj):
+        animals = Animal.objects.filter(farm=obj)
+        need = 0
+        # raise Exception
+        for a in animals:
+            need += a.type.krahmal
+        return need
+
+    def calories_needs(self, obj):
+        animals = Animal.objects.filter(farm=obj)
+        need = 0
+        # raise Exception
+        for a in animals:
+            need += a.type.calories
+        return need
+
+    def sugar_needs(self, obj):
+        animals = Animal.objects.filter(farm=obj)
+        need = 0
+        # raise Exception
+        for a in animals:
+            need += a.type.sugar
+        return need
+
+    def calcii_needs(self, obj):
+        animals = Animal.objects.filter(farm=obj)
+        need = 0
+        # raise Exception
+        for a in animals:
+            need += a.type.calcii
+        return need
+
+    def fosfor_needs(self, obj):
+        animals = Animal.objects.filter(farm=obj)
+        need = 0
+        # raise Exception
+        for a in animals:
+            need += a.type.fosfor
+        return need
+
+    def magnie_needs(self, obj):
+        animals = Animal.objects.filter(farm=obj)
+        need = 0
+        # raise Exception
+        for a in animals:
+            need += a.type.magnie
+        return need
+
+    def cali_needs(self, obj):
+        animals = Animal.objects.filter(farm=obj)
+        need = 0
+        # raise Exception
+        for a in animals:
+            need += a.type.cali
+        return need
+
+    def sera_needs(self, obj):
+        animals = Animal.objects.filter(farm=obj)
+        need = 0
+        # raise Exception
+        for a in animals:
+            need += a.type.sera
+        return need
+
+    def jelezo_needs(self, obj):
+        animals = Animal.objects.filter(farm=obj)
+        need = 0
+        # raise Exception
+        for a in animals:
+            need += a.type.jelezo
+        return need
+
+    def med_needs(self, obj):
+        animals = Animal.objects.filter(farm=obj)
+        need = 0
+        # raise Exception
+        for a in animals:
+            need += a.type.med
+        return need
+
+    def cink_needs(self, obj):
+        animals = Animal.objects.filter(farm=obj)
+        need = 0
+        # raise Exception
+        for a in animals:
+            need += a.type.cink
+        return need
+
+    def marganec_needs(self, obj):
+        animals = Animal.objects.filter(farm=obj)
+        need = 0
+        # raise Exception
+        for a in animals:
+            need += a.type.marganec
+        return need
+
+    def kobalt_needs(self, obj):
+        animals = Animal.objects.filter(farm=obj)
+        need = 0
+        # raise Exception
+        for a in animals:
+            need += a.type.kobalt
+        return need
+
+    def iod_needs(self, obj):
+        animals = Animal.objects.filter(farm=obj)
+        need = 0
+        # raise Exception
+        for a in animals:
+            need += a.type.iod
+        return need
+
+    def karotin_needs(self, obj):
+        animals = Animal.objects.filter(farm=obj)
+        need = 0
+        # raise Exception
+        for a in animals:
+            need += a.type.med
+        return need
+
+    def vit_e_needs(self, obj):
+        animals = Animal.objects.filter(farm=obj)
+        need = 0
+        # raise Exception
+        for a in animals:
+            need += a.type.vit_e
+        return need
+
+    def vit_d_needs(self, obj):
+        animals = Animal.objects.filter(farm=obj)
+        need = 0
+        # raise Exception
+        for a in animals:
+            need += a.type.vit_d
+        return need
+
+    def vit_a_needs(self, obj):
+        animals = Animal.objects.filter(farm=obj)
+        need = 0
+        # raise Exception
+        for a in animals:
+            need += a.type.vit_a
+        return need
+
     readonly_fields = (
         'company',
-
+        'calories_needs',
+        'krahmal_needs',
+        'sugar_needs',
+        'calcii_needs',
+        'fosfor_needs',
+        'magnie_needs',
+        'cali_needs',
+        'sera_needs',
+        'jelezo_needs',
+        'med_needs',
+        'cink_needs',
+        'marganec_needs',
+        'kobalt_needs',
+        'iod_needs',
+        'karotin_needs',
+        'vit_e_needs',
+        'vit_d_needs',
+        'vit_a_needs'
     )
-    fields = (
-        'company',
-        'name',
+    # fields = (
+    #     'company',
+    #     'krahmal_needs',
+    #     'name',
+    # )
+    fieldset_info = ('Standart_info', {'fields':
+                                           ('company', 'name')
+                                       })
+
+    fieldset_needs = ('Needs', {'fields':
+                                    ('calories_needs',
+                                     'krahmal_needs',
+                                     'sugar_needs',
+                                     'calcii_needs',
+                                     'fosfor_needs',
+                                     'magnie_needs',
+                                     'cali_needs',
+                                     'sera_needs',
+                                     'jelezo_needs',
+                                     'med_needs',
+                                     'cink_needs',
+                                     'marganec_needs',
+                                     'kobalt_needs',
+                                     'iod_needs',
+                                     'karotin_needs',
+                                     'vit_e_needs',
+                                     'vit_d_needs',
+                                     'vit_a_needs',
+                                     )
+                                })
+
+    fieldsets = (
+        fieldset_info,
+        fieldset_needs
     )
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
@@ -93,9 +285,6 @@ class FarmAdmin(ModelAdmin):
 
     def has_add_permission(self, request):
         return True
-
-
-
 
     def has_change_permission(self, request, obj=None):
         return True
--- management/models/food.py
+++ management/models/food.py
@@ -4,3 +4,25 @@ from django.db import models
 class Food(models.Model):
     name = models.CharField(max_length=100)
     calorie = models.IntegerField()
+
+    krahmal = models.IntegerField(null=True, blank=True, default=0)
+    sugar = models.IntegerField(null=True, blank=True, default=0)
+    calcii = models.IntegerField(null=True, blank=True, default=0)
+    fosfor = models.IntegerField(null=True, blank=True, default=0)
+    magnie = models.IntegerField(null=True, blank=True, default=0)
+    cali = models.IntegerField(null=True, blank=True, default=0)
+    sera = models.IntegerField(null=True, blank=True, default=0)
+    jelezo = models.IntegerField(null=True, blank=True, default=0)
+    med = models.IntegerField(null=True, blank=True, default=0)
+    cink = models.IntegerField(null=True, blank=True, default=0)
+    marganec = models.IntegerField(null=True, blank=True, default=0)
+    kobalt = models.IntegerField(null=True, blank=True, default=0)
+    iod = models.IntegerField(null=True, blank=True, default=0)
+    karotin = models.IntegerField(null=True, blank=True, default=0)
+
+    vit_e = models.IntegerField(null=True, blank=True, default=0)
+    vit_d = models.IntegerField(null=True, blank=True, default=0)
+    vit_a = models.IntegerField(null=True, blank=True, default=0)
+
+    # Cost
+    cost = models.IntegerField(null=True, blank=True, default=0)
