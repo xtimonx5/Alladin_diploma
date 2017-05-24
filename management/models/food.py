b85b1b3a0192fdd5131b4767320c15d1725328ac
Dmitry Gavrilenko <d.gavrilenko@invento.by>
2017-05-24 11:27:07 +0300

    foo

--- management/models/food.py
+++ management/models/food.py
@@ -3,30 +3,36 @@ from django.db import models
 
 class Food(models.Model):
     name = models.CharField(max_length=100)
-    calorie = models.IntegerField()
 
-    krahmal = models.IntegerField(null=True, blank=True, default=0)
-    sugar = models.IntegerField(null=True, blank=True, default=0)
-    calcii = models.IntegerField(null=True, blank=True, default=0)
-    fosfor = models.IntegerField(null=True, blank=True, default=0)
-    magnie = models.IntegerField(null=True, blank=True, default=0)
-    cali = models.IntegerField(null=True, blank=True, default=0)
-    sera = models.IntegerField(null=True, blank=True, default=0)
-    jelezo = models.IntegerField(null=True, blank=True, default=0)
-    med = models.IntegerField(null=True, blank=True, default=0)
-    cink = models.IntegerField(null=True, blank=True, default=0)
-    marganec = models.IntegerField(null=True, blank=True, default=0)
-    kobalt = models.IntegerField(null=True, blank=True, default=0)
-    iod = models.IntegerField(null=True, blank=True, default=0)
-    karotin = models.IntegerField(null=True, blank=True, default=0)
+    сorm = models.FloatField(verbose_name='Корм')
+    calorie = models.FloatField(verbose_name='Обмен энергией')
+    suh_veschestvo = models.FloatField(null=True, blank=True, default=0, verbose_name='Сухое вещество')
+    Siro_proteine = models.FloatField(null=True, blank=True, default=0, verbose_name='Сырой протеин')
+    Perev_proteine = models.FloatField(null=True, blank=True, default=0, verbose_name='Переваренный протеин')
+    sir_jir = models.FloatField(null=True, blank=True, default=0, verbose_name='Сыр жир')
+    sir_cletchatka = models.FloatField(null=True, blank=True, default=0, verbose_name='Сыр клетчатка')
 
-    vit_e = models.IntegerField(null=True, blank=True, default=0)
-    vit_d = models.IntegerField(null=True, blank=True, default=0)
-    vit_a = models.IntegerField(null=True, blank=True, default=0)
+    krahmal = models.FloatField(null=True, blank=True, default=0, verbose_name='Крахмал')
+    sugar = models.FloatField(null=True, blank=True, default=0, verbose_name='Сахар')
+    sol = models.FloatField(null=True, blank=True, default=0, verbose_name='Соль')
 
-    # Cost
-    cost = models.IntegerField(null=True, blank=True, default=0)
+    calcii = models.FloatField(null=True, blank=True, default=0, verbose_name='Кальций')
+    fosfor = models.FloatField(null=True, blank=True, default=0, verbose_name='Фосфор')
+    magnie = models.FloatField(null=True, blank=True, default=0, verbose_name='Магний')
+    cali = models.FloatField(null=True, blank=True, default=0, verbose_name='Калий')
+    sera = models.FloatField(null=True, blank=True, default=0, verbose_name='Сера')
+    jelezo = models.FloatField(null=True, blank=True, default=0, verbose_name='Железо')
+    natri = models.FloatField(null=True, blank=True, default=0, verbose_name='Натрий')
+    med = models.FloatField(null=True, blank=True, default=0, verbose_name='Медь')
+    cink = models.FloatField(null=True, blank=True, default=0, verbose_name='Цинк')
+    marganec = models.FloatField(null=True, blank=True, default=0, verbose_name='Марганец')
+    kobalt = models.FloatField(null=True, blank=True, default=0, verbose_name='Кобальт ')
+    iod = models.FloatField(null=True, blank=True, default=0, verbose_name='Йод')
+    karotin = models.FloatField(null=True, blank=True, default=0, verbose_name='Каротин')
 
+    vit_e = models.FloatField(null=True, blank=True, default=0, verbose_name='Витамин Е')
+    vit_d = models.FloatField(null=True, blank=True, default=0, verbose_name='Витамин Д')
+    vit_a = models.FloatField(null=True, blank=True, default=0, verbose_name='Витамин А')
 
     def __str__(self):
         return self.name
